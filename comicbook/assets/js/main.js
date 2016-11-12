68388462e053d06cc82417c31e9eedc1525cc40e
Fr0st91 <bojicrs9@gmail.com>
2016-11-12 01:08:22 +0100

    testzikaa

--- comicbook/assets/js/main.js
+++ comicbook/assets/js/main.js
@@ -1,34 +1,5 @@
 $(document).ready(function(){
 
-	var truth=false;
-	var audio1 = document.createElement('audio');
-	audio1.setAttribute('src','sound1.mp3');
-	
-	var condition1=false;
-	var condition2=false;
-	var condition3=false;
-	var condition4=false;
-	
-        $('#prva').hide();
-		$('#prva').fadeIn(2700);
-		$('#druga').hide();
-		$('#treca').hide();
-		$('#cetvrta').hide();
-		
-		
-		$(document).keydown(function(e) {
-			if(e.which > 0) {
-				$('#druga').fadeIn(2700);
-				truth=true;
-			}
-			if(truth==true) {
-		$(document).keydown(function(e) {
-			if(e.which > 0) {
-				$('#treca').fadeIn(2700);
-				truth=false;
-			}
-		}); }
-	});
-		
-		
+	$('.comicPart1').hide();
+
 }); 
