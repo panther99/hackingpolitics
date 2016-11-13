ee57d666a49479af1f2dc094e58bee2e387f4e93
Miroslav Pavlov <Miroslav Pavlov>
2016-11-13 16:04:39 +0100

    stranka slike

--- comicbook/zika.js
+++ comicbook/zika.js
@@ -69,3 +69,25 @@ function pojava() {
    // jQuery methods go here...
 
 //Tajmer se ovako pise, a ovo izmedju "start" i "end" unutar tajmera nisam siguran ali po toj logici sam mislio da se radi
+
+
+	/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
